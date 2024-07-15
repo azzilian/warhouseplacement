1b651e71a4071a7d71e185e31a1287c69983a9ef
azzilian <crusotdeath@gmail.com>
2024-07-15 19:55:05 +0300

    Minor Fixes - magical numbers created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,11 @@ import org.example.FindOptimalPoint;
 import org.example.service.MapGenerator;
 
 public class Main {
+    public static final int X_COORDINATE_INDEX = 0;
+    public static final int Y_COORDINATE_INDEX = 1;
+    public static final int ROWS_COUNT = 4;
+    public static final int COLUMNS_COUNT = 5;
+
     private static FindOptimalPoint findOptimalPoint;
 
     public static void main(String[] args) {
@@ -20,16 +25,16 @@ public class Main {
 
         int[] optimalPoint = FindOptimalPoint.findOptimalPlacement(residents);
         System.out.println("Optimal placement for warehouse is at: ("
-                + optimalPoint[0]
+                + optimalPoint[X_COORDINATE_INDEX]
                 + ", "
-                + optimalPoint[1]
+                + optimalPoint[Y_COORDINATE_INDEX]
                 + ")");
 
-        int[] optimalPointRandom = FindOptimalPoint.findOptimalPlacement(MapGenerator.generateRandomRectangularMap(4,5));
+        int[] optimalPointRandom = FindOptimalPoint.findOptimalPlacement(MapGenerator.generateRandomRectangularMap(ROWS_COUNT, COLUMNS_COUNT));
         System.out.println("Optimal placement for warehouse is at: ("
-                + optimalPointRandom[0]
+                + optimalPointRandom[X_COORDINATE_INDEX]
                 + ", "
-                + optimalPointRandom[1]
+                + optimalPointRandom[Y_COORDINATE_INDEX]
                 + ")");
     }
 }
